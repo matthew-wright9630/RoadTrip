6d88d16a2891c0dd69da36cfd2c26075755e4eff
matthew-wright9630 <matthew.wright9630@gmail.com>
2024-06-14 23:53:12 -0400

    Commit after creating normalize.css

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      // favicon: "./src/images/favicon.ico",
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
